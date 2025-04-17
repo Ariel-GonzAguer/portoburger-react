7a391c85c39d7ec8ea3ef18764caa9e292a9cde6
Ariel-GonzAguer <ariegonzaguer@gmail.com>
2025-04-17 16:06:47 -0600

    feat: update WhatsApp redirection URL and include total in message after 5 seconds

--- src/pages/pago-ficticio.tsx
+++ src/pages/pago-ficticio.tsx
@@ -5,17 +5,17 @@ import useStorePorto from "../state/useStorePorto"
 import styles from "../styles/Pago-Ficticio.module.css"
 
 // hooks
-import { useEffect } from "react"
+// import { useEffect } from "react"
 
 
 export default function PagoFicticio() {
   const { carrito } = useStorePorto()
 
-  useEffect(() => {
-    setTimeout(() => {
-      window.location.href = "https://wa.me/50660450629?text=Hola%20me%20gustaría%20hacer%20un%20pedido%20de%20los%20siguientes%20productos:%20" + carrito.map((item) => item.nombre + " x " + item.cantidad + " = ₡" + item.precio * item.cantidad).join("%0A")
-    }, 5000)
-  }, [])
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     window.location.href = "https://wa.me/50666666666?text=Hola%20me%20gustaría%20hacer%20un%20pedido%20de%20los%20siguientes%20productos:%20" + carrito.map((item) => item.nombre + " x " + item.cantidad + " = ₡" + item.precio * item.cantidad).join("%0A") + "%0A%0ATotal:%20₡" + carrito.reduce((total, item) => total + item.precio * item.cantidad, 0)
+  //   }, 5000)
+  // }, [])
 
   return (
     <section className={styles.pagoFicticio}>
